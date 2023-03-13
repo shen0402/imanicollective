2bff764e3178f0ada29908b0727eed97d15dba05
shen0402 <shenzaijun0402@gmail.com>
2023-03-13 16:45:25 -0700

    theme init

---
+++ assets/swatchify-data.js
@@ -0,0 +1,8 @@
+
+(function() {
+  if(!window['SellerPanda'] || !window['SellerPanda']['Swatchify']){
+    console.error('Swatchify error! Please contact support@sellerpanda.com')
+  } else {
+    window['SellerPanda']['Swatchify'].swatchify_SetSwatches({swatches: {"Color":{"Tan":"c#D2B48C","Blue":"c#0000FF","Onyx":"c#000000","Pink":"c#E8B39C","Rust":"c#AF7552","Black":"c#000000","Peach":"c#EAC394","Taupe":"c#CEB5A0","White":"c#FFFFFF","black":"c#000000","Cognac":"c#C58442","Natural":"c#F1E3D5","natural":"c#F1E3D5","Lavendar":"c#EEDAEA","Lavender":"c#EEDAEA","sunshine":"c#EAC394","Checkered Taupe":"c#CEB5A0"}}, specific: {}});
+  }
+}())
