1dbcc5506eb6c00fbf3dfc08af05175c9b527e25
shen0402 <shenzaijun0402@gmail.com>
2023-03-20 17:32:36 -0700

    update blog template

--- assets/custom.js
+++ assets/custom.js
@@ -10,3 +10,22 @@ $(document).on('click', '.quantity-icon', function(){
     }
     $input.val(qty);
 });
+
+$(document).ready(function(){
+    var limit_blog = 7;
+    var current_show_blogs = limit_blog;
+    if ($('.blog-item').length > limit_blog) {
+        $('.blog-button').removeClass('hide');
+        $('.blog-button').click(function(){
+            current_show_blogs += limit_blog;
+            $('.blog-item').each(function(index){
+                if (index < current_show_blogs) {
+                    $(this).removeClass('hide');
+                }
+                if (current_show_blogs >= $('.blog-item').length) {
+                    $('.blog-button').addClass('hide');
+                }
+            });
+        });
+    }
+});
